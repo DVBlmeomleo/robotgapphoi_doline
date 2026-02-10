02d4b117e9591957e77ef1d9c74a5c98333a90d5
DVBlmeomleo <dvbachpg@gmail.com>
2026-02-10 10:38:07 +0700

    Update doline.ino

--- doline.ino
+++ doline.ino
@@ -17,10 +17,9 @@
 int speed = 500;
 int v1, v2, v3, v4, v5;
 int direction = 0;
-bool turn_1 = false;
-int turn_4;
 int turn = 0;
 int pre_error;
+int P,I,D, kp, ki, kd;
 
 void setup() {
   pinMode(ir_1, INPUT);
@@ -37,22 +36,90 @@ void setup() {
   pinMode(motor_enb, OUTPUT);
 }
 
-void loop() {
-  check_ir();
-  int error = error_calc();
-  if (pre_error != error){
-    move_direction();
-  pre_error = error;
-  delay(500);
+
+//mấy hàm dưới này cứ để đấy đã, sửa sau, nhìn tên hàm ra chức năng để xem logic đã
+void dithang(){
+  digitalWrite(motor_in1, HIGH);
+  digitalWrite(motor_in2, HIGH);
+  digitalWrite(motor_in3, HIGH);
+  digitalWrite(motor_in4, HIGH);
+  analogWrite(motor_ena, speed);
+  analogWrite(motor_enb, speed);
+}
+void dung(){
+  digitalWrite(motor_in1, HIGH);
+  digitalWrite(motor_in2, HIGH);
+  digitalWrite(motor_in3, HIGH);
+  digitalWrite(motor_in4, HIGH);
+  analogWrite(motor_ena, speed);
+  analogWrite(motor_enb, speed);
+}
+void retrai(){
+  digitalWrite(motor_in1, HIGH);
+  digitalWrite(motor_in2, HIGH);
+  digitalWrite(motor_in3, HIGH);
+  digitalWrite(motor_in4, HIGH);
+  analogWrite(motor_ena, speed);
+  analogWrite(motor_enb, speed);
+}
+void rephai(){
+  digitalWrite(motor_in1, HIGH);
+  digitalWrite(motor_in2, HIGH);
+  digitalWrite(motor_in3, HIGH);
+  digitalWrite(motor_in4, HIGH);
+  analogWrite(motor_ena, speed);
+  analogWrite(motor_enb, speed);
+}
+void dilui(){
+  digitalWrite(motor_in1, HIGH);
+  digitalWrite(motor_in2, HIGH);
+  digitalWrite(motor_in3, HIGH);
+  digitalWrite(motor_in4, HIGH);
+  analogWrite(motor_ena, speed);
+  analogWrite(motor_enb, speed);
+}
+void quaydau(){
+  digitalWrite(motor_in1, HIGH);
+  digitalWrite(motor_in2, HIGH);
+  digitalWrite(motor_in3, HIGH);
+  digitalWrite(motor_in4, HIGH);
+  analogWrite(motor_ena, speed);
+  analogWrite(motor_enb, speed);
+}
+void dichphai(){
+  digitalWrite(motor_in1, HIGH);
+  digitalWrite(motor_in2, HIGH);
+  digitalWrite(motor_in3, HIGH);
+  digitalWrite(motor_in4, HIGH);
+  analogWrite(motor_ena, speed);
+  analogWrite(motor_enb, speed);
+}
+void dichtrai(){
+  digitalWrite(motor_in1, HIGH);
+  digitalWrite(motor_in2, HIGH);
+  digitalWrite(motor_in3, HIGH);
+  digitalWrite(motor_in4, HIGH);
+  analogWrite(motor_ena, speed);
+  analogWrite(motor_enb, speed);
+}
+
+void check_ir(){
+  v1 = digitalRead(ir_1); //1 la ngoai line, 0 la trong line  (hoac nguoc lai, tuy de)
+  v2 = digitalRead(ir_2);
+  v3 = digitalRead(ir_3);
+  v4 = digitalRead(ir_4);
+  v5 = digitalRead(ir_5);
 }
 
 int turn_3 = 0, turn_4 = 0, turn_7 = 0, turn_11 = 0, turn_12_a = 0, turn_12_b = 0, turn_13_a = 0, turn_13_b = 0, turn_14 = 0, turn_14_a = 0;
-void move_direction(){
-  switch (turn){ //từ case 1 đến 9 là gắp phôi vuông và di chuyển đến vị trí thả đồ, từ case 10 đổ đi là bắt đầu về gắp phôi tròn và hoàn thành
+void move_direction(int error){
+  switch (turn){
     case 0:
       if (error == 10){
         dithang();
-      }else{turn++;}
+      }else{
+        turn++;
+      }
       break;
     case 1:
       if (error == 12){
@@ -150,6 +217,7 @@ void move_direction(){
       if (v1 == il && v5 == il){
         rephai();
         turn++;
+      }
       break;
     case 10:
       if (error != 13){
@@ -198,14 +266,14 @@ void move_direction(){
     case 13:
       if (v1 == il){
         if (v5 == il){
-          turn_13++;
-          if (turn_13 == 3){
+          turn_13_a++;
+          if (turn_13_a == 3){
             rephai();
           }
         }
         retrai();
       }else if (error == 13){
-        if (turn_13_a == 1){
+        if (turn_13_b == 1){
           turn++;
         }
         turn_13_b++;
@@ -223,16 +291,17 @@ void move_direction(){
           }else{
             rephai();
           }
-      }else if (v5 == il){
-        rephai();
-      }else if(error == 13){
-        if (turn_14_a == 1){
-          turn++;
+        }else if (v5 == il){
+          rephai();
+        }else if(error == 13){
+          if (turn_14_a == 1){
+            turn++;
+          }
+          turn_14_a++;
+          quaydau();
+        }else{
+          dithang();
         }
-        turn_14_a++;
-        quaydau();
-      }else{
-        dithang();
       }
       break;
     case 15:
@@ -248,14 +317,6 @@ void move_direction(){
   }
 }
 
-void check_ir(){
-  v1 = digitalRead(s1); //1 la ngoai line, 0 la trong line  (hoac nguoc lai, tuy de)
-  v2 = digitalRead(s2);
-  v3 = digitalRead(s3);
-  v4 = digitalRead(s4);
-  v5 = digitalRead(s5);
-}
-
 int error_calc(){
   if (v1 == ol && v2 == ol && v3 == ol && v4 == ol && v5 == il){return -4;}
   else if (v1 == ol && v2 == ol && v3 == ol && v4 == il && v5 == il){return -3;}
@@ -266,87 +327,19 @@ int error_calc(){
   else if (v1 == ol && v2 == il && v3 == ol && v4 == ol && v5 == ol){return 2;}
   else if (v1 == il && v2 == il && v3 == ol && v4 == ol && v5 == ol){return 3;}
   else if (v1 == il && v2 == ol && v3 == ol && v4 == ol && v5 == ol){return 4;}
-
   else if (v1 == ol && v2 == ol && v3 == ol && v4 == ol && v5 == ol){return 9;}
   else if (v1 == il && v5 == il){return 10;}
   else if (v1 == il && v2 == il && v3 == il && v4 == ol && v5 == ol){return 11;}
   else if (v1 == ol && v2 == ol && v3 == il && v4 == il && v5 == il){return 12;}
   else{return 50;}
-
-}
-//kệ hàm pid_calc đi, chưa cần đến
-int pid_calc(int error){
-  // error = error_calc();
-  P = error;
-  I += error;
-  D -= previous_error;
-  int pid_value_1 = kp*P + ki*I + kd*D;
-  previous_error = error;
-  return pid_value_1;
 }
 
-//mấy hàm dưới này cứ để đấy đã, sửa sau, nhìn tên hàm ra chức năng để xem logic đã
-void dithang(){
-  digitalWrite(motor_in1, HIGH);
-  digitalWrite(motor_in2, HIGH);
-  digitalWrite(motor_in3, HIGH);
-  digitalWrite(motor_in4, HIGH);
-  analoglWrite(motor_ena, speed);
-  analogWrite(motor_enb, speed);
-}
-void dung(){
-  digitalWrite(motor_in1, HIGH);
-  digitalWrite(motor_in2, HIGH);
-  digitalWrite(motor_in3, HIGH);
-  digitalWrite(motor_in4, HIGH);
-  analoglWrite(motor_ena, speed);
-  analogWrite(motor_enb, speed);
-}
-void retrai(){
-  digitalWrite(motor_in1, HIGH);
-  digitalWrite(motor_in2, HIGH);
-  digitalWrite(motor_in3, HIGH);
-  digitalWrite(motor_in4, HIGH);
-  analoglWrite(motor_ena, speed);
-  analogWrite(motor_enb, speed);
-}
-void rephai(){
-  digitalWrite(motor_in1, HIGH);
-  digitalWrite(motor_in2, HIGH);
-  digitalWrite(motor_in3, HIGH);
-  digitalWrite(motor_in4, HIGH);
-  analoglWrite(motor_ena, speed);
-  analogWrite(motor_enb, speed);
-}
-void dilui(){
-  digitalWrite(motor_in1, HIGH);
-  digitalWrite(motor_in2, HIGH);
-  digitalWrite(motor_in3, HIGH);
-  digitalWrite(motor_in4, HIGH);
-  analoglWrite(motor_ena, speed);
-  analogWrite(motor_enb, speed);
-}
-void quaydau(){
-  digitalWrite(motor_in1, HIGH);
-  digitalWrite(motor_in2, HIGH);
-  digitalWrite(motor_in3, HIGH);
-  digitalWrite(motor_in4, HIGH);
-  analoglWrite(motor_ena, speed);
-  analogWrite(motor_enb, speed);
-}
-void dichphai(){
-  digitalWrite(motor_in1, HIGH);
-  digitalWrite(motor_in2, HIGH);
-  digitalWrite(motor_in3, HIGH);
-  digitalWrite(motor_in4, HIGH);
-  analoglWrite(motor_ena, speed);
-  analogWrite(motor_enb, speed);
-}
-void dichtrai(){
-  digitalWrite(motor_in1, HIGH);
-  digitalWrite(motor_in2, HIGH);
-  digitalWrite(motor_in3, HIGH);
-  digitalWrite(motor_in4, HIGH);
-  analoglWrite(motor_ena, speed);
-  analogWrite(motor_enb, speed);
+void loop() {
+  check_ir();
+  int error = error_calc();
+  if (pre_error != error){
+    move_direction(error);
+  }
+  pre_error = error;
+  delay(500);
 }
